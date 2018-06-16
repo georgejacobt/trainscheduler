d8868d5df3478f6b4ed3b4ebaac68be0542fdecf
George Jacob Work <36488006+georgejacobt@users.noreply.github.com>
2018-06-16 01:07:25 -0500

    after auth page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-
 let row = 0;
 
 let currentTime = moment();
@@ -6,18 +5,53 @@ console.log(currentTime);
 
 
 
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyBYRFPIf8n1FSldXjkNHSyn6acyUXcrZYc",
+//     authDomain: "trainapp-99104.firebaseapp.com",
+//     databaseURL: "https://trainapp-99104.firebaseio.com",
+//     projectId: "trainapp-99104",
+//     storageBucket: "trainapp-99104.appspot.com",
+//     messagingSenderId: "816275816192"
+//   };
+//   firebase.initializeApp(config);
+
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBYRFPIf8n1FSldXjkNHSyn6acyUXcrZYc",
-    authDomain: "trainapp-99104.firebaseapp.com",
-    databaseURL: "https://trainapp-99104.firebaseio.com",
-    projectId: "trainapp-99104",
-    storageBucket: "trainapp-99104.appspot.com",
-    messagingSenderId: "816275816192"
+    apiKey: "AIzaSyCM6vMnWxAYLmwzHBizhZLIQEQF6iz_H-c",
+    authDomain: "informationdb-974af.firebaseapp.com",
+    databaseURL: "https://informationdb-974af.firebaseio.com",
+    projectId: "informationdb-974af",
+    storageBucket: "informationdb-974af.appspot.com",
+    messagingSenderId: "937471337573"
   };
   firebase.initializeApp(config);
 
+
+
+
+
   var database = firebase.database();
+
+//   var uiConfig = {
+//     signInSuccessUrl: 'https://www.google.com',
+//     signInOptions: [
+//       // Leave the lines as is for the providers you want to offer your users.
+//       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//     //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+//     //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+//     //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
+//     //   firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//     //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
+//     ],
+//     // Terms of service url.
+//     tosUrl: '<your-tos-url>'
+//   };
+
+//   // Initialize the FirebaseUI Widget using Firebase.
+//   var ui = new firebaseui.auth.AuthUI(firebase.auth());
+//   // The start method will wait until the DOM is loaded.
+//   ui.start('#firebaseui-auth-container', uiConfig);
 
   let dbName = "";
   let dbDestination = "";
@@ -77,6 +111,74 @@ tr.append(td5);
     }
 });
 
+// setInterval(updateTimes,10000)
+
+// function updateTimes(){
+//     console.log("here")
+
+//     database.ref().once("value", function(snapshot) {
+
+//         for (var key in snapshot.val()) {
+    
+//             let tr = $("<tr>");
+//             let th = $("<th>");
+//             let td1 = $("<td>");
+//             let td2 = $("<td>");
+//             let td3 = $("<td>");
+//             let td4 = $("<td>");
+//             let td5 = $("<td>");
+    
+//              //console.log(key);
+    
+//             console.log(snapshot.child(key).val().trainName);
+//             dbName = snapshot.child(key).val().trainName;
+//             console.log(snapshot.child(key).val().destination);
+//             dbDestination = snapshot.child(key).val().destination;
+//             console.log(snapshot.child(key).val().frequency);
+//             dbFrequency = snapshot.child(key).val().frequency;
+//             dbNextArrivalTime = snapshot.child(key).val().nextArrivalTime;
+//             dbMinsAway = snapshot.child(key).val().minutesAway;
+//             dbMinsAway = dbMinsAway -1;
+
+//             database.ref(key).update({
+//                 minutesAway: dbMinsAway
+
+//             });
+    
+//             console.log(dbMinsAway);
+    
+            
+//     $("tbody").append(tr);
+//     th.attr("scope","row");
+//     row++ ;
+//     th.text(row);
+//     tr.append(th);
+    
+//     td1.text(dbName);
+//     tr.append(td1);
+    
+//     td2.text(dbDestination);
+//     tr.append(td2);
+    
+//     td3.text(dbFrequency);
+//     tr.append(td3);
+    
+//     td4.text(dbNextArrivalTime);
+//     tr.append(td4);
+    
+//     td5.text(dbMinsAway);
+//     tr.append(td5);
+            
+    
+    
+//         }
+//     });
+// }
+
+
+
+
+
 
 
 
@@ -97,29 +199,47 @@ let trainName = $("#trainName").val();
 let destination = $("#dest").val();
 let firsTrain = $("#firstTime").val();
 let freq = parseInt($("#freq").val());
-console.log(firsTrain);
+//console.log(firsTrain);
 let firsTrainConv = moment(firsTrain, "HHmm");
 let firstTrainValue = moment(firsTrain, "HHmm").format("hh:mm a");
 let checkFuture = firsTrainConv.diff(moment(),"minutes");
-let nextArrival ="";
-let minsAway= "";
-
-
-    nextArrival = firstTrainValue;
-
-   //nextArrival = moment(firsTrainConv).add(freq,"m").format("hh:mm a");
-
-
-let nextArrivalObj = moment(firsTrainConv).add(freq,"m");
-
-if (checkFuture > 0){
-    minsAway = checkFuture;
+let minsFromFirst = moment().diff(firsTrainConv,"minutes");
+let actualMinsAway = "";
+let actualNextArrival = "";
+console.log(minsFromFirst);
+if (minsFromFirst > 0){
+    let modulus = minsFromFirst % freq;
+actualMinsAway = freq - modulus;
+console.log("Minutes Away:"+actualMinsAway);
+actualNextArrival = moment().add(actualMinsAway,"m").format("HH:mm a");
+console.log("Next Arrival1:"+actualNextArrival);
+} else{
+    actualMinsAway = -(minsFromFirst);
+    console.log("Minutes Away:"+actualMinsAway);
+    actualNextArrival = firstTrainValue;
+    console.log("Next Arrival2:"+actualNextArrival);
 }
-else minsAway = 1440 + checkFuture;
+
+//console.log(minsFromFirst);
+// let nextArrival ="";
+// let minsAway= "";
+
+
+//     nextArrival = firstTrainValue;
+
+//    //nextArrival = moment(firsTrainConv).add(freq,"m").format("hh:mm a");
+
+
+// let nextArrivalObj = moment(firsTrainConv).add(freq,"m");
+
+// if (checkFuture > 0){
+//     minsAway = checkFuture;
+// }
+// else minsAway = 1440 + checkFuture;
 
 
 
-console.log(minsAway);
+//console.log(minsAway);
 
 
 
@@ -142,8 +262,8 @@ database.ref().push({
     trainName: trainName,
     destination: destination,
     frequency: freq,
-    nextArrivalTime: nextArrival,
-    minutesAway: minsAway
+    nextArrivalTime: actualNextArrival,
+    minutesAway: actualMinsAway
 
 });
 
